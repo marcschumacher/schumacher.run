6674d6425107a912d263a6c19c18bf9e16da01de
Marc Schumacher <mail@marc-schumacher.de>
2016-11-14 23:16:41 +0100

    Initial version

---
+++ gradle/config/buildConfig.groovy
@@ -0,0 +1,37 @@
+environments {
+
+  local {
+    server {
+      host = "localhost"
+      port = 8085
+      contextPath = "/blog/"
+      url  = "http://$host:$port"
+    }
+  }
+
+  test {
+    server {
+      host = "zweiblum"
+      contextPath = "/blog/"
+      url = "https://$host"
+    }
+    ssh {
+      host = "zweiblum"
+      user = "frank"
+      destination = "/var/www/blog"
+    }
+  }
+
+  prod {
+    server {
+      host = "www.calmdevelopment.de"
+      contextPath = "/blog/"
+      url = "https://$host"
+    }
+    ssh {
+      host = "calmdev"
+      user = "t0ken"
+      destination = "/home/t0ken/html/blog"
+    }
+  }
+}
